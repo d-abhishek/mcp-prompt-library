62a7572062c28b4a6bb3024d26b6923fd5fd9d13
Abhishek Dhekane <adhekane@stud.hs-heilbronn.de>
2025-11-08 20:50:11 +0100

    refactor(app): remove old CdkStack - clean up imports

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -3,7 +3,6 @@ import os
 
 import aws_cdk as cdk
 
-from cdk.cdk_stack import CdkStack
 from cdk.mcp_stack import McpCdkStack
 
 app = cdk.App()
@@ -11,9 +10,6 @@ app = cdk.App()
 account = os.getenv("CDK_DEFAULT_ACCOUNT")
 region  = os.getenv("CDK_DEFAULT_REGION")
 
-CdkStack(app, "McpInfraStack",
-        env=cdk.Environment(account=account, region=region),
-)
 McpCdkStack(app, "McpServerStack",
         env=cdk.Environment(account=account, region=region),
 )
